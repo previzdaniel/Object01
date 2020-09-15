6357942ace21c0cdf3dbf95db5e6c009add3bef7
previzdaniel <dani.2000.2x@gmail.com>
2020-09-15 10:20:12 +0200

    OOP kezdetek

--- Object01/Program.cs
+++ Object01/Program.cs
@@ -22,7 +22,36 @@ namespace Object01
         }
     }
 
+    class teglalap
+    {
+        public int a;
+        public int b;
 
+        public int Kerulet()
+        {
+            return 2 * (a + b);
+        }
+
+        public int Terulet()
+        {
+            return a * b;
+        }
+    }
+
+    class kor
+    {
+        public int r;
+
+        public double Kerulet()
+        {
+            return 2 * r * Math.PI;
+        }
+
+        public double Terulet()
+        {
+            return Math.Pow(r,2) * Math.PI;
+        }
+    }
 
     class Program
     {
@@ -41,6 +70,23 @@ namespace Object01
 
             Console.WriteLine(Pisti.Bemutatkozas());
             Console.WriteLine(Eva.Bemutatkozas());
+
+            teglalap t = new teglalap();
+
+            Console.Write("A téglalap 'a' oldala: ");
+            t.a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("A téglalap 'b' oldala: ");
+            t.b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Téglalap vagyok, terület: {t.Terulet()}, kerület: {t.Kerulet()}.");
+
+            kor k = new kor();
+
+            Console.Write("A kör kerülete: ");
+            k.r = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"A kör kerülete: {k.Kerulet()}, területe: {k.Terulet()}");
+            
 
             Console.ReadKey();
         }
